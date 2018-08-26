b48a5e2498c6111cdbd30e99b528f01bd84570a9
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-08-26 14:08:19 -0500

    Post message to slack channel with Lambda function is triggered

--- notifier/handler.py
+++ notifier/handler.py
@@ -1,9 +1,12 @@
 import os
+import requests
 
 
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']  # this matches the serverless.yml environment variable
-    print(slack_webhook_url)
-    print(event)
-    
+
+    # can get the full event message by printing the event to cloudwatch
+    slack_message = f"From {event['source']} at {event['detail']['StartTime']}: {event['detail']['Description']}\nReason: {event['detail']['Cause']}"
+    data = { "text": slack_message }
+    requests.post(slack_webhook_url, json=data)
     return
